1c21fa6b367c7f73e24f03365031fea5092e2040
Aidan Bailey <dev@aidanbailey.me>
2024-07-17 10:41:47 +0200

    Add Children trait

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ pub mod trie;
 
 #[cfg(test)]
 mod tests {
-    use crate::trie::{Node, Trie};
+    use crate::trie::{Node, Trie, Children};
 
     #[test]
     fn trie_new() {
@@ -14,5 +14,14 @@ mod tests {
     fn node_new() {
         let node = Node::new(1);
         assert_eq!(node.key(), &1);
+    }
+
+    #[test]
+    fn node_children_mut() {
+        let mut node = Node::new(1);
+        node.children_mut().push(Node::new(2));
+        assert_eq!(node.children().len(), 1);
+        node.children_mut().clear();
+        assert_eq!(node.children().len(), 0);
     }
 }
--- src/trie.rs
+++ src/trie.rs
@@ -1,16 +1,29 @@
 /// Trie node
 pub struct Node<KT: Ord> {
-    key: KT
+    key: KT,
+    children: Vec<Node<KT>>,
 }
 
 impl<KT: Ord> Node<KT> {
     /// Construct a node with a key
     pub fn new(key: KT) -> Node<KT> {
-        Node { key }
+        Node {
+            key,
+            children: vec![],
+        }
     }
-    /// Get the key
     pub fn key(&self) -> &KT {
         &self.key
+    }
+}
+
+impl<KT: Ord> Children<KT> for Node<KT> {
+    fn children(&self) -> &Vec<Node<KT>> {
+        &self.children
+    }
+
+    fn children_mut(&mut self) -> &mut Vec<Node<KT>> {
+        &mut self.children
     }
 }
 
@@ -24,8 +37,22 @@ impl<KT: Ord> Trie<KT> {
     pub fn new() -> Trie<KT> {
         Trie { children: vec![] }
     }
-    /// Returns true iff the Trie has no children
-    pub fn is_empty(&self) -> bool {
-        self.children.is_empty()
+}
+
+impl<KT: Ord> Children<KT> for Trie<KT> {
+    fn children(&self) -> &Vec<Node<KT>> {
+        &self.children
+    }
+    fn children_mut(&mut self) -> &mut Vec<Node<KT>> {
+        &mut self.children
+    }
+}
+
+pub trait Children<KT: Ord> {
+    fn children(&self) -> &Vec<Node<KT>>;
+    fn children_mut(&mut self) -> &mut Vec<Node<KT>>;
+    /// Returns true iff the Node has no children
+    fn is_empty(&self) -> bool {
+        self.children().is_empty()
     }
 }
