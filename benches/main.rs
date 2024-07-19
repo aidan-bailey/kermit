149e3b3666b38a595a7be11ae51d2c915337607e
Aidan Bailey <dev@aidanbailey.me>
2024-07-19 19:57:05 +0200

    Cut unused benchmark param

--- benches/main.rs
+++ benches/main.rs
@@ -7,15 +7,11 @@ fn generate_vector<T: PartialOrd + SampleUniform + Copy>(
     arity: usize,
     min: T,
     max: T,
-    sorted: bool,
 ) -> Vec<T> {
     let mut rng = rand::thread_rng();
     let mut vector = Vec::<T>::new();
     for _ in 0..arity {
         vector.push(rng.gen_range(min..max));
-    }
-    if sorted {
-        vector.sort_unstable_by(|a, b| a.partial_cmp(b).unwrap());
     }
     vector
 }
@@ -25,7 +21,7 @@ fn generate_tuples<T: PartialOrd + SampleUniform + Copy>(
 ) -> Vec<Vec<T>> {
     let mut vectors = Vec::<Vec<T>>::new();
     while vectors.len() < params.size {
-        let vector = generate_vector(params.arity, params.min, params.max, params.sorted);
+        let vector = generate_vector(params.arity, params.min, params.max);
         if !vectors.contains(&vector) {
             vectors.push(vector);
         }
@@ -38,41 +34,39 @@ struct BenchParams<T: PartialOrd + SampleUniform + Copy> {
     arity: usize,
     min: T,
     max: T,
-    sorted: bool,
 }
 
 impl<T: PartialOrd + SampleUniform + Copy + std::fmt::Display> BenchParams<T> {
-    fn new(size: usize, arity: usize, min: T, max: T, sorted: bool) -> BenchParams<T> {
+    fn new(size: usize, arity: usize, min: T, max: T) -> BenchParams<T> {
         BenchParams {
             size,
             arity,
             min,
             max,
-            sorted,
         }
     }
 
     fn to_string(&self) -> String {
-        format!("size: {}, arity: {}, min: {}, max: {}, sorted: {}", self.size, self.arity, self.min, self.max, self.sorted)
+        format!("size: {}, arity: {}, min: {}, max: {}", self.size, self.arity, self.min, self.max)
     }
 }
 
 pub fn criterion_benchmark(c: &mut Criterion) {
     let mut group = c.benchmark_group("insertion");
     let bench_params = vec![
-        BenchParams::new(2, 1000, 0, 1000000, false),
-        BenchParams::new(4, 1000, 0, 1000000, false),
-        BenchParams::new(8, 1000, 0, 1000000, false),
-        BenchParams::new(16, 1000, 0, 1000000, false),
-        BenchParams::new(32, 1000, 0, 1000000, false),
-        BenchParams::new(64, 1000, 0, 1000000, false),
-        BenchParams::new(128, 1000, 0, 1000000, false),
-        BenchParams::new(256, 1000, 0, 1000000, false),
-        BenchParams::new(512, 1000, 0, 1000000, false),
-        BenchParams::new(1024, 1000, 0, 1000000, false),
-        BenchParams::new(2048, 1000, 0, 1000000, false),
-        BenchParams::new(4096, 1000, 0, 1000000, false),
-        BenchParams::new(8192, 1000, 0, 1000000, false),
+        BenchParams::new(2, 1000, 0, 1000000),
+        BenchParams::new(4, 1000, 0, 1000000),
+        BenchParams::new(8, 1000, 0, 1000000),
+        BenchParams::new(16, 1000, 0, 1000000),
+        BenchParams::new(32, 1000, 0, 1000000),
+        BenchParams::new(64, 1000, 0, 1000000),
+        BenchParams::new(128, 1000, 0, 1000000),
+        BenchParams::new(256, 1000, 0, 1000000),
+        BenchParams::new(512, 1000, 0, 1000000),
+        BenchParams::new(1024, 1000, 0, 1000000),
+        BenchParams::new(2048, 1000, 0, 1000000),
+        BenchParams::new(4096, 1000, 0, 1000000),
+        BenchParams::new(8192, 1000, 0, 1000000),
     ];
 
     for bench_param in bench_params {
