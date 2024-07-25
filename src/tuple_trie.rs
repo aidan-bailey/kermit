8d864d206e5f3ed1cd7d41f422b3cddaa8420fa7
Aidan Bailey <dev@aidanbailey.me>
2024-07-25 22:43:22 +0200

    Add presort

--- src/tuple_trie.rs
+++ src/tuple_trie.rs
@@ -38,6 +38,24 @@ impl<KT: PartialOrd + PartialEq> Trie<KT> {
         trie
     }
 
+    pub fn from_tuples_presort(arity: usize, mut tuples: Vec<Vec<KT>>) -> Trie<KT> {
+        tuples.sort_by(|a, b| {
+            for i in 0..a.len() {
+                if a[i] < b[i] {
+                    return std::cmp::Ordering::Less;
+                } else if a[i] > b[i] {
+                    return std::cmp::Ordering::Greater;
+                }
+            }
+            std::cmp::Ordering::Equal
+        });
+        let mut trie = Trie::new(arity);
+        for tuple in tuples {
+            trie.insert(tuple).unwrap();
+        }
+        trie
+    }
+
     pub fn insert(&mut self, tuple: Vec<KT>) -> Result<(), &'static str> {
         if tuple.len() != self.arity {
             return Err("Arity doesn't match.");
