811cd10482269146e57a9a16d692fc1a5bbd461a
Aidan Bailey <dev@aidanbailey.me>
2024-07-27 10:24:43 +0200

    Add test

--- kermit-algos/tests/lib.rs
+++ kermit-algos/tests/lib.rs
@@ -38,4 +38,25 @@ mod tests {
             assert_eq!(triejoin.key.unwrap(), i);
         }
     }
+
+    #[test]
+    fn join3() {
+        let trie_a = TrieBuilder::<i32>::new(1).from_file("tests/data/col_a.csv").unwrap().build();
+        let trie_b = TrieBuilder::<i32>::new(1).from_file("tests/data/col_b.csv").unwrap().build();
+        let trie_c = TrieBuilder::<i32>::new(1).from_file("tests/data/col_c.csv").unwrap().build();
+        let mut iter_a = TrieIter::new(&trie_a);
+        iter_a.open().unwrap();
+        let mut iter_b = TrieIter::new(&trie_b);
+        iter_b.open().unwrap();
+        let mut iter_c = TrieIter::new(&trie_c);
+        iter_c.open().unwrap();
+        let mut triejoin = LeapfrogTriejoinIter::new(vec![iter_a, iter_b, iter_c]);
+        assert_eq!(triejoin.key.unwrap(), 7);
+        triejoin.next().expect("Hello");
+        assert_eq!(triejoin.key.unwrap(), 10);
+        triejoin.next().expect("Hello");
+        assert_eq!(triejoin.key.unwrap(), 20);
+        triejoin.next().expect("Hello");
+        assert!(triejoin.at_end());
+    }
 }
