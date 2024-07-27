c59663c72c483cf714475ddb33cbe9417fc3c8cb
Aidan Bailey <dev@aidanbailey.me>
2024-07-27 18:34:48 +0200

    Doc

--- kermit-iters/src/trie.rs
+++ kermit-iters/src/trie.rs
@@ -1,26 +1,56 @@
-/// Trie iterator interface.
+/// Trie iterator trait
 pub trait TrieIterator<KT: PartialOrd + PartialEq + Clone> {
-    /// Returns key of a Node iff not at the end or at Root.
+
+    /// Returns a reference to the key if
+    /// the iterator is positioned at a
+    /// non-root node, otherwise None.
     fn key(&self) -> Option<&KT>;
-    /// Proceeds to the next key
+
+    /// Moves the iterator forward and returns
+    /// a reference to the key if the iterator
+    /// is positioned at a non-root node, otherwise
+    /// None.
     fn next(&mut self) -> Option<&KT>;
-    /// Position the iterator at a least
-    /// upper bound for seekKey,
-    /// i.e. the least key ≥ seekKey, or
-    /// move to end if no such key exists.
-    /// The sought key must be ≥ the
-    /// key at the current position.
+
+    /// Positions the iterator at a least
+    /// upper bound for seek_key,
+    /// i.e. the least key ≥ seek_key,
+    /// and returns a reference to the key, or
+    /// move to end if no such key exists and
+    /// returns None.
+    ///
+    /// # Panics
+    ///
+    /// If the seek_key is not ≥ the key at the
+    /// current position.
     fn seek(&mut self, seek_key: &KT) -> Option<&KT>;
-    /// True when iterator is at the end.
+
+    /// Returns true iff the iterator is positioned
+    /// at the end.
     fn at_end(&self) -> bool;
-    /// Proceed to the first key at the
-    /// next depth
+
+    /// If there is a non-root node at the iterator's current position which has children,
+    /// positions the iterator at the first child and returns a reference to the key.
+    /// Otherwise, returns None.
+    ///
+    /// # Note
+    /// If the iterator is positioned at the end, then this functions as if
+    /// the iterator is positioned at the previous node.
     fn open(&mut self) -> Option<&KT>;
-    /// Return to the parent key at the
-    /// previous depth
+
+    /// If there is a non-root node at the iterator's current position,
+    /// positions the iterator at its parent and returns a reference to the key
+    /// if it is a non-root Node.
+    /// Otherwise, returns None.
+    ///
+    /// # Note
+    /// If the iterator is positioned at the end, then this functions as if
+    /// the iterator is positioned at the previous node.
     fn up(&mut self) -> Option<&KT>;
+
 }
 
+/// Trie iterable trait
 pub trait TrieIterable<KT: PartialOrd + PartialEq + Clone> {
     fn trie_iter(&self) -> impl TrieIterator<KT>;
 }
