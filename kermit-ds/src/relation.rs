f8c83e5f0954537a90122833119c03f4f12dad03
Aidan Bailey <dev@aidanbailey.me>
2024-08-03 19:22:21 +0200

    Refactor

--- kermit-ds/src/relation.rs
+++ kermit-ds/src/relation.rs
@@ -1,36 +1,56 @@
+use std::ops::Index;
+
+pub trait Relational<KT: Clone> : Index<usize> {
+    fn cardinality(&self) -> usize;
+    fn tuples(&self) -> &Vec<Vec<KT>>;
+    fn insert(&mut self, tuple: Vec<KT>);
+    fn insert_all(&mut self, tuples: Vec<Vec<KT>>);
+    fn clear(&mut self);
+}
+
+impl<KT: PartialOrd + PartialEq + Clone> Index<usize> for Relation<KT> {
+    type Output = Vec<KT>;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.tuples[index]
+    }
+}
+
 #[derive(Clone)]
 pub struct Relation<KT: Clone> {
-    name: String,
     cardinality: usize,
-    tuples: Vec<Box<[KT]>>,
+    tuples: Vec<Vec<KT>>,
 }
 
 impl<KT: Clone> Relation<KT> {
-    pub fn new(name: String, cardinality: usize) -> Self {
+    pub fn new(cardinality: usize) -> Self {
         Relation {
-            name,
             cardinality,
             tuples: Vec::new(),
         }
-    }
-
-    pub fn name(&self) -> &str {
-        &self.name
     }
 
     pub fn cardinality(&self) -> usize {
         self.cardinality
     }
 
-    pub fn tuples(&self) -> &Vec<Box<[KT]>> {
+    pub fn tuples(&self) -> &Vec<Vec<KT>> {
         &self.tuples
     }
 
-    pub fn insert(&mut self, tuple: Box<[KT]>) {
+    pub fn insert(&mut self, tuple: Vec<KT>) {
+        if tuple.len() != self.cardinality {
+            panic!("Tuple has wrong cardinality");
+        }
         self.tuples.push(tuple);
     }
 
-    pub fn insert_all(&mut self, tuples: Vec<Box<[KT]>>) {
+    pub fn insert_all(&mut self, tuples: Vec<Vec<KT>>) {
+        for tuple in tuples.iter() {
+            if tuple.len() != self.cardinality {
+                panic!("Tuple has wrong cardinality");
+            }
+        }
         self.tuples.extend(tuples);
     }
 
