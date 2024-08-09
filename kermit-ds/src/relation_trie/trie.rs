c96aa27c394c80c6d406717fe43b1fe57a8c2e79
Aidan Bailey <dev@aidanbailey.me>
2024-08-09 19:53:30 +0200

    Doc

--- kermit-ds/src/relation_trie/trie.rs
+++ kermit-ds/src/relation_trie/trie.rs
@@ -1,28 +1,53 @@
 use crate::relation_trie::node::{Internal, Node, TrieFields};
 
-/// Trie
+/// Trie data structure for relations.
 #[derive(Clone, Debug)]
 pub struct RelationTrie<KT>
 where
     KT: PartialOrd + PartialEq + Clone,
 {
+    /// Cardinality of the trie.
     cardinality: usize,
+    /// Children of the trie root.
     children: Vec<Node<KT>>,
 }
 
+/// Trie implementation.
 impl<KT> RelationTrie<KT>
 where
     KT: PartialOrd + PartialEq + Clone,
 {
-    /// Construct an empty Trie
+    /// Construct an empty Trie.
+    ///
+    /// # Examples
+    /// ```
+    /// use trie_rs::relation_trie::RelationTrie;
+    /// let trie = RelationTrie::<usize>::new(2);
+    /// ```
+    ///
+    /// # Panics
+    /// If `cardinality` is less than 1.
     pub fn new(cardinality: usize) -> RelationTrie<KT> {
+        assert!(cardinality > 0, "Cardinality must be greater than 0.");
         RelationTrie {
             cardinality,
             children: vec![],
         }
     }
 
+    /// Construct a Trie from a list of tuples.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use trie_rs::relation_trie::RelationTrie;
+    /// let trie = RelationTrie::<usize>::from_tuples(2, vec![vec![1, 2], vec![3, 4]]);
+    /// ```
+    ///
+    /// # Panics
+    /// If any tuple does not have a matching `cardinality`.
     pub fn from_tuples(cardinality: usize, tuples: Vec<Vec<KT>>) -> RelationTrie<KT> {
+        assert!(tuples.iter().all(|tuple| tuple.len() == cardinality));
         let mut trie = RelationTrie::new(cardinality);
         for tuple in tuples {
             trie.insert(tuple).unwrap();
@@ -30,6 +55,17 @@ where
         trie
     }
 
+    // TODO: Rename this method
+    /// Construct a Trie from a list of tuples.
+    ///
+    /// # Examples
+    /// ```
+    /// use trie_rs::relation_trie::RelationTrie;
+    /// let trie = RelationTrie::<usize>::from_tuples_presort(2, vec![vec![1, 2], vec![3, 4]]);
+    /// ```
+    ///
+    /// # Panics
+    /// If any tuple does not have a matching `cardinality`.
     pub fn from_tuples_presort(cardinality: usize, mut tuples: Vec<Vec<KT>>) -> RelationTrie<KT> {
         tuples.sort_unstable_by(|a, b| {
             for i in 0..a.len() {
@@ -41,13 +77,10 @@ where
             }
             std::cmp::Ordering::Equal
         });
-        let mut trie = RelationTrie::new(cardinality);
-        for tuple in tuples {
-            trie.insert(tuple).unwrap();
-        }
-        trie
+        RelationTrie::from_tuples(cardinality, tuples)
     }
 
+    /// Insert a tuple into the Trie.
     pub fn insert(&mut self, tuple: Vec<KT>) -> Result<(), &'static str> {
         if tuple.len() != self.cardinality {
             return Err("Arity doesn't match.");
