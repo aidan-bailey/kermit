6f6588831dde9c864e435d546ec634a894ed77fd
Aidan Bailey <dev@aidanbailey.me>
2024-08-09 20:21:16 +0200

    Doc

--- kermit-algos/src/leapfrog_triejoin.rs
+++ kermit-algos/src/leapfrog_triejoin.rs
@@ -1,12 +1,31 @@
 use {kermit_iters::trie::TrieIterator, std::marker::PhantomData};
 
 pub trait LeapfrogTriejoinIterator<KT: PartialOrd + PartialEq + Clone> {
+
+    /// Initializes the iterator.
     fn init(&mut self) -> Option<&KT>;
+
+    /// Proceed to the next key.
     fn next(&mut self) -> Option<&KT>;
+
+    /// Proceed to the next matching key.
     fn search(&mut self) -> Option<&KT>;
+
+    /// Position the iterator at a least
+    /// upper bound for seekKey,
+    /// i.e. the least key ≥ seekKey, or
+    /// move to end if no such key exists.
+    /// The sought key must be ≥ the
+    /// key at the current position.
     fn seek(&mut self, seek_key: &KT) -> Option<&KT>;
+
+    /// Check if the iterator is at the end.
     fn at_end(&self) -> bool;
+
+    /// Proceed to the first key at the next depth.
     fn open(&mut self) -> Option<&KT>;
+
+    /// Proceed to the parent key at the previous depth.
     fn up(&mut self) -> Option<&KT>;
 }
 
