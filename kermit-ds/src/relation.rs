4e432f3bad4c33e41d0d4735cc5c67fdb3068880
Aidan Bailey <dev@aidanbailey.me>
2024-08-15 13:37:26 +0200

    Create relation.rs

---
+++ kermit-ds/src/relation.rs
@@ -0,0 +1,9 @@
+pub trait Relation<KT>
+where
+    KT: PartialOrd + PartialEq + Clone,
+{
+    fn cardinality(&self) -> usize;
+    fn insert(&mut self, tuple: Vec<KT>) -> bool;
+    fn insert_all(&mut self, tuples: Vec<Vec<KT>>) -> bool;
+    fn from_tuples(cardinality: usize, tuples: Vec<Vec<KT>>) -> Self;
+}
