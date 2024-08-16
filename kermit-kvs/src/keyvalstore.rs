89a4be895f6c954090e72dfea6ece37f16766835
Aidan Bailey <dev@aidanbailey.me>
2024-08-16 10:06:25 +0200

    Refactor

--- kermit-kvs/src/keyvalstore.rs
+++ kermit-kvs/src/keyvalstore.rs
@@ -1,12 +1,12 @@
 use std::hash::Hash;
 
-trait KeyValStore<KT, VT>
+pub trait KeyValStore<KT, VT>
 where
-    KT: PartialEq + PartialOrd + Clone,
+    KT: Eq + Hash + Clone + PartialOrd,
     VT: Hash,
 {
     fn add(&mut self, val: VT) -> KT;
     fn add_all(&mut self, val: Vec<VT>) -> Vec<KT>;
-    fn get(&self, key: KT) -> Option<VT>;
-    fn get_all(&self, key: Vec<KT>) -> Vec<VT>;
+    fn get(&self, key: &KT) -> Option<&VT>;
+    fn get_all(&self, key: Vec<&KT>) -> Vec<Option<&VT>>;
 }
