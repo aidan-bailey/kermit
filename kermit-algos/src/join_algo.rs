228c1ba80909f3eed83c60758ff22f7639da76c7
Aidan Bailey <dev@aidanbailey.me>
2025-05-29 19:19:02 +0200

    Cut more unneeded lifetimes

--- kermit-algos/src/join_algo.rs
+++ kermit-algos/src/join_algo.rs
@@ -1,11 +1,11 @@
 use kermit_iters::trie::Iterable;
 
-pub trait JoinAlgo<'a, KT, ITB>
+pub trait JoinAlgo<KT, ITB>
 where
-    KT: Ord + Clone + 'a,
+    KT: Ord + Clone,
     ITB: Iterable<KT>,
 {
     fn join(
-        variables: Vec<usize>, rel_variables: Vec<Vec<usize>>, iterables: Vec<&'a ITB>,
+        variables: Vec<usize>, rel_variables: Vec<Vec<usize>>, iterables: Vec<&ITB>,
     ) -> Vec<Vec<KT>>;
 }
