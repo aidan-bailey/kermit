8994bc5e2cb72e87f50292dbac446b2a915d730a
Aidan Bailey <dev@aidanbailey.me>
2025-05-31 21:22:30 +0200

    Add generic database join function

--- kermit/tests/tests.rs
+++ kermit/tests/tests.rs
@@ -1,4 +1,5 @@
 use {
+    kermit::db::Database,
     kermit_algos::{join_algo::JoinAlgo, leapfrog_triejoin::LeapfrogTriejoin},
     kermit_ds::{
         relation::Relation,
@@ -6,7 +7,8 @@ use {
         relation_trie::{trie::RelationTrie, trie_builder::TrieBuilder},
     },
     kermit_iters::trie::Iterable,
-    std::{cmp::PartialOrd, fmt::Debug, str::FromStr},
+    kermit_kvs::{anyvaltype::AnyValType, keyvalstore::KeyValStore, naivestore::NaiveStore},
+    std::{cmp::PartialOrd, fmt::Debug, hash::Hash, str::FromStr},
 };
 
 fn compute_join<KT, R, RB, JA>(
@@ -40,4 +42,51 @@ fn test_simple_join() {
     let variables = vec![0];
     let rel_variables = vec![vec![0], vec![0]];
     compute_join::<KT, R, RB, JA>(arity, inputs, variables, rel_variables);
+}
+
+fn compute_db_join<KT, VT, KVST, R, RB, JA>(
+    input1: Vec<Vec<KT>>, input2: Vec<Vec<KT>>,
+) -> kermit::db::Database<KT, VT, KVST, R, RB>
+where
+    KT: Debug + FromStr + PartialOrd + PartialEq + Clone + Hash + std::cmp::Eq + Ord,
+    KVST: KeyValStore<KT, VT> + Default,
+    VT: Hash,
+    R: Relation<KT> + Iterable<KT>,
+    RB: RelationBuilder<KT, R>,
+    JA: JoinAlgo<KT, R>,
+{
+    let mut db = Database::<KT, VT, KVST, R, RB>::new("test_db".to_string(), KVST::default());
+
+    db.add_relation("first", 1);
+    db.add_keys_batch("first", input1);
+
+    db.add_relation("second", 1);
+    db.add_keys_batch("second", input2);
+
+    let _result = db.join::<JA>(
+        vec!["first".to_string(), "second".to_string()],
+        vec![0],
+        vec![vec![0], vec![0]],
+    );
+
+    db
+}
+
+#[test]
+fn test_db_creation() {
+    // Assuming you have a concrete implementation of KeyValStore, Relation, and
+    // RelationBuilder Replace `ConcreteKVStore`, `ConcreteRelation`, and
+    // `ConcreteRelationBuilder` with actual types
+    compute_db_join::<
+        u64,
+        AnyValType,
+        NaiveStore<_, std::hash::BuildHasherDefault<std::hash::DefaultHasher>>,
+        RelationTrie<_>,
+        TrieBuilder<_>,
+        LeapfrogTriejoin,
+    >(vec![vec![1_u64], vec![2], vec![3]], vec![
+        vec![1_u64],
+        vec![2],
+        vec![3],
+    ]);
 }
