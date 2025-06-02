9c305b6c6850215b5619bf1c246425b61c9b2aa4
Aidan Bailey <dev@aidanbailey.me>
2025-06-02 15:56:48 +0200

    Restructure exports

--- kermit/src/lib.rs
+++ kermit/src/lib.rs
@@ -1,17 +1,16 @@
 pub mod algos {
-    pub use kermit_algos::*;
-}
-
-pub mod ds {
-    pub use kermit_ds::*;
-}
-
-pub mod iters {
-    pub use kermit_iters::*;
+    pub use kermit_algos::leapfrog_triejoin::LeapfrogTriejoin;
 }
 
 pub mod kvs {
-    pub use kermit_kvs::*;
+    pub use kermit_kvs::{anyvaltype::AnyValType, naivestore::NaiveStore};
+}
+
+pub mod ds {
+    pub use kermit_ds::relation_trie::{trie::RelationTrie, trie_builder::TrieBuilder};
 }
 
 pub mod db;
+
+mod utils;
+pub use utils::{compute_db_join, compute_join};
--- kermit/tests/tests.rs
+++ kermit/tests/tests.rs
@@ -1,32 +1,9 @@
-use {
-    kermit::db::Database,
-    kermit_algos::{join_algo::JoinAlgo, leapfrog_triejoin::LeapfrogTriejoin},
-    kermit_ds::{
-        relation::Relation,
-        relation_builder::RelationBuilder,
-        relation_trie::{trie::RelationTrie, trie_builder::TrieBuilder},
-    },
-    kermit_iters::trie::Iterable,
-    kermit_kvs::{anyvaltype::AnyValType, keyvalstore::KeyValStore, naivestore::NaiveStore},
-    std::{cmp::PartialOrd, fmt::Debug, hash::Hash, str::FromStr},
+use kermit::{
+    algos::LeapfrogTriejoin,
+    compute_db_join, compute_join,
+    ds::{RelationTrie, TrieBuilder},
+    kvs::{AnyValType, NaiveStore},
 };
-
-fn compute_join<KT, R, RB, JA>(
-    arity: usize, input: Vec<Vec<Vec<KT>>>, variables: Vec<usize>, rel_variables: Vec<Vec<usize>>,
-) -> Vec<Vec<KT>>
-where
-    KT: PartialOrd + std::clone::Clone + Ord + Debug + FromStr,
-    R: Relation<KT> + Iterable<KT>,
-    RB: RelationBuilder<KT, R>,
-    JA: JoinAlgo<KT, R>,
-{
-    let relations: Vec<_> = input
-        .into_iter()
-        .map(|tuples| RB::new(arity).add_tuples(tuples).build())
-        .collect();
-    let iterables = relations.iter().collect::<Vec<_>>();
-    JA::join(variables, rel_variables, iterables)
-}
 
 #[test]
 fn test_simple_join() {
@@ -44,39 +21,8 @@ fn test_simple_join() {
     compute_join::<KT, R, RB, JA>(arity, inputs, variables, rel_variables);
 }
 
-fn compute_db_join<KT, VT, KVST, R, RB, JA>(
-    input1: Vec<Vec<KT>>, input2: Vec<Vec<KT>>,
-) -> kermit::db::Database<KT, VT, KVST, R, RB>
-where
-    KT: Debug + FromStr + PartialOrd + PartialEq + Clone + Hash + std::cmp::Eq + Ord,
-    KVST: KeyValStore<KT, VT> + Default,
-    VT: Hash,
-    R: Relation<KT> + Iterable<KT>,
-    RB: RelationBuilder<KT, R>,
-    JA: JoinAlgo<KT, R>,
-{
-    let mut db = Database::<KT, VT, KVST, R, RB>::new("test_db".to_string(), KVST::default());
-
-    db.add_relation("first", 1);
-    db.add_keys_batch("first", input1);
-
-    db.add_relation("second", 1);
-    db.add_keys_batch("second", input2);
-
-    let _result = db.join::<JA>(
-        vec!["first".to_string(), "second".to_string()],
-        vec![0],
-        vec![vec![0], vec![0]],
-    );
-
-    db
-}
-
 #[test]
 fn test_db_creation() {
-    // Assuming you have a concrete implementation of KeyValStore, Relation, and
-    // RelationBuilder Replace `ConcreteKVStore`, `ConcreteRelation`, and
-    // `ConcreteRelationBuilder` with actual types
     compute_db_join::<
         u64,
         AnyValType,
