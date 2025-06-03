82f4a468b73253a2d70f52e418638f1b0ba3843f
Aidan Bailey <git@aidanbailey.me>
2025-06-03 17:07:28 +0200

    Doc

--- kermit-ds/src/relation.rs
+++ kermit-ds/src/relation.rs
@@ -1,9 +1,20 @@
+/// ! This module defines the `Relation` trait.
 use crate::key_type::KeyType;
 
+/// Trait for relations.
 pub trait Relation {
+    /// The type of value stored in a relation's tuples.
     type KT: KeyType;
+
+    /// Returns the cardinality of the relation, which is the number of tuples
+    /// it contains.
     fn cardinality(&self) -> usize;
+
+    /// Inserts a tuple into the relation, returning `true` if successful and
+    /// `false` if otherwise.
     fn insert(&mut self, tuple: Vec<Self::KT>) -> bool;
+
+    /// Inserts multiple tuples into the relation, returning `true` if
+    /// successful and `false` if otherwise.
     fn insert_all(&mut self, tuples: Vec<Vec<Self::KT>>) -> bool;
-    // fn from_tuples(cardinality: usize, tuples: Vec<Vec<KT>>) -> Self;
 }
