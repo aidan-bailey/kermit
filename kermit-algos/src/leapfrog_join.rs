d9e41b2aaeed960003946959dd5bed503d0e0667
Aidan Bailey <dev@aidanbailey.me>
2025-06-06 17:13:34 +0200

    Add leapfrog join

---
+++ kermit-algos/src/leapfrog_join.rs
@@ -0,0 +1,255 @@
+use kermit_iters::{key_type::KeyType, linear::LinearIterator};
+
+pub trait LeapfrogJoinIterator<'a> {
+    type KT: KeyType;
+
+    fn key(&self) -> Option<&'a Self::KT>;
+
+    fn leapfrog_init(&mut self) -> Option<&'a Self::KT>;
+
+    fn leapfrog_search(&mut self) -> Option<&'a Self::KT>;
+
+    fn leapfrog_next(&mut self) -> Option<&'a Self::KT>;
+
+    fn at_end(&self) -> bool;
+}
+
+pub struct LeapfrogJoinIter<'a, IT>
+where
+    IT: LinearIterator<'a>,
+{
+    key: Option<&'a IT::KT>,
+    iterators: Vec<IT>,
+    p: usize,
+}
+
+impl<'a, IT> LeapfrogJoinIter<'a, IT>
+where
+    IT: LinearIterator<'a>,
+{
+    pub fn new(iterators: Vec<IT>) -> Self {
+        LeapfrogJoinIter {
+            key: None,
+            iterators,
+            p: 0,
+        }
+    }
+
+    pub fn k(&self) -> usize { self.iterators.len() }
+}
+
+impl<'a, IT> LeapfrogJoinIterator<'a> for LeapfrogJoinIter<'a, IT>
+where
+    IT: LinearIterator<'a> + 'a,
+{
+    type KT = IT::KT;
+
+    fn key(&self) -> Option<&'a Self::KT> { self.key }
+
+    fn leapfrog_init(&mut self) -> Option<&'a Self::KT> {
+
+        for iter in &mut self.iterators {
+            iter.next();
+        }
+
+        if self.at_end() {
+            return None;
+        }
+
+        self.iterators.sort_unstable_by(|a, b| {
+            let a_key = a.key().unwrap();
+            let b_key = b.key().unwrap();
+            if a_key < b_key {
+                std::cmp::Ordering::Less
+            } else if a_key > b_key {
+                std::cmp::Ordering::Greater
+            } else {
+                std::cmp::Ordering::Equal
+            }
+        });
+
+        self.p = 0;
+        self.leapfrog_search()
+    }
+
+    fn leapfrog_search(&mut self) -> Option<&'a Self::KT> {
+        let prime_i = if self.p == 0 {
+            self.k() - 1
+        } else {
+            self.p - 1
+        };
+        let mut x_prime = self.iterators[prime_i].key()?;
+        loop {
+            let x = self.iterators[self.p].key()?;
+            if x == x_prime {
+                self.key = Some(x);
+                return Some(x);
+            } else {
+                self.iterators[self.p].seek(x_prime);
+                if self.iterators[self.p].at_end() {
+                    return None;
+                } else {
+                    x_prime = self.iterators[self.p].key()?;
+                    self.p = (self.p + 1) % self.k();
+                }
+            }
+        }
+    }
+
+    fn leapfrog_next(&mut self) -> Option<&'a Self::KT> {
+        self.iterators[self.p].next();
+        if self.iterators[self.p].at_end() {
+            return None;
+        } else {
+            self.p = (self.p + 1) % self.k();
+            self.leapfrog_search()
+        }
+    }
+
+    fn at_end(&self) -> bool {
+        for iter in &self.iterators {
+            if iter.at_end() {
+                return true;
+            }
+        }
+        false
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use kermit_iters::linear::LinearIterable;
+
+    #[test]
+    fn test_leapfrog_join_iter() {
+        let v1 = vec![1, 2, 3];
+        let v2 = vec![2, 3, 4];
+
+        let mut join_iter = LeapfrogJoinIter::new(vec![v1.linear_iter(), v2.linear_iter()]);
+
+        assert_eq!(join_iter.leapfrog_init(), Some(&2));
+        assert_eq!(join_iter.leapfrog_next(), Some(&3));
+        assert_eq!(join_iter.leapfrog_next(), None);
+    }
+
+    #[test]
+    fn test_leapfrog_join_iter_empty() {
+        let v1: Vec<i32> = vec![];
+        let v2: Vec<i32> = vec![];
+
+        let mut join_iter = LeapfrogJoinIter::new(vec![v1.linear_iter(), v2.linear_iter()]);
+
+        assert_eq!(join_iter.leapfrog_init(), None);
+    }
+
+    #[test]
+    fn test_leapfrog_join_iter_no_common_elements() {
+        let v1 = vec![1, 3, 5];
+        let v2 = vec![2, 4, 6];
+
+        let mut join_iter = LeapfrogJoinIter::new(vec![v1.linear_iter(), v2.linear_iter()]);
+
+        assert_eq!(join_iter.leapfrog_init(), None);
+    }
+
+    #[test]
+    fn test_leapfrog_join_iter_single_iterator() {
+        let v1 = vec![1, 2, 3];
+
+        let mut join_iter = LeapfrogJoinIter::new(vec![v1.linear_iter()]);
+
+        assert_eq!(join_iter.leapfrog_init(), Some(&1));
+    }
+
+    #[test]
+    fn test_leapfrog_join_iter_multiple_vectors_with_common_elements() {
+        let v1 = vec![1, 2, 3, 5];
+        let v2 = vec![2, 4, 5, 6];
+        let v3 = vec![2, 5, 7];
+
+        let mut join_iter = LeapfrogJoinIter::new(vec![v1.linear_iter(), v2.linear_iter(), v3.linear_iter()]);
+
+        assert_eq!(join_iter.leapfrog_init(), Some(&2));
+        assert_eq!(join_iter.leapfrog_next(), Some(&5));
+        assert_eq!(join_iter.leapfrog_next(), None);
+    }
+
+    #[test]
+    fn test_leapfrog_join_iter_no_common_elements_multiple_vectors() {
+        let v1 = vec![1, 3, 5];
+        let v2 = vec![2, 4, 6];
+        let v3 = vec![7, 8, 9];
+
+        let mut join_iter = LeapfrogJoinIter::new(vec![v1.linear_iter(), v2.linear_iter(), v3.linear_iter()]);
+
+        assert_eq!(join_iter.leapfrog_init(), None);
+    }
+
+    #[test]
+    fn test_leapfrog_join_iter_empty_and_non_empty() {
+        let v1: Vec<i32> = vec![];
+        let v2 = vec![1, 2, 3];
+
+        let mut join_iter = LeapfrogJoinIter::new(vec![v1.linear_iter(), v2.linear_iter()]);
+
+        assert_eq!(join_iter.leapfrog_init(), None);
+    }
+
+    #[test]
+    fn test_leapfrog_join_iter_multiple_empty_vectors() {
+        let v1: Vec<i32> = vec![];
+        let v2: Vec<i32> = vec![];
+        let v3: Vec<i32> = vec![];
+
+        let mut join_iter = LeapfrogJoinIter::new(vec![v1.linear_iter(), v2.linear_iter(), v3.linear_iter()]);
+
+        assert_eq!(join_iter.leapfrog_init(), None);
+    }
+
+    #[test]
+    fn test_leapfrog_join_iter_single_common_element() {
+        let v1 = vec![1, 2, 3];
+        let v2 = vec![2];
+
+        let mut join_iter = LeapfrogJoinIter::new(vec![v1.linear_iter(), v2.linear_iter()]);
+
+        assert_eq!(join_iter.leapfrog_init(), Some(&2));
+        assert_eq!(join_iter.leapfrog_next(), None);
+    }
+
+    #[test]
+    fn test_leapfrog_join_iter_multiple_vectors_with_duplicates() {
+        let v1 = vec![1, 2, 2, 3];
+        let v2 = vec![2, 4, 4];
+        let v3 = vec![2, 5, 7];
+
+        let mut join_iter = LeapfrogJoinIter::new(vec![v1.linear_iter(), v2.linear_iter(), v3.linear_iter()]);
+
+        assert_eq!(join_iter.leapfrog_init(), Some(&2));
+        assert_eq!(join_iter.leapfrog_next(), None);
+    }
+
+    #[test]
+    fn test_leapfrog_join_iter_large_vectors() {
+        let v1: Vec<i32> = (1..1000).collect();
+        let v2: Vec<i32> = (500..1500).collect();
+
+        let mut join_iter = LeapfrogJoinIter::new(vec![v1.linear_iter(), v2.linear_iter()]);
+
+        assert_eq!(join_iter.leapfrog_init(), Some(&500));
+        assert_eq!(join_iter.leapfrog_next(), Some(&501));
+    }
+
+    #[test]
+    fn test_leapfrog_join_iter_multiple_vectors_with_one_empty() {
+        let v1 = vec![1, 2, 3];
+        let v2: Vec<i32> = vec![];
+        let v3 = vec![2, 4, 6];
+
+        let mut join_iter = LeapfrogJoinIter::new(vec![v1.linear_iter(), v2.linear_iter(), v3.linear_iter()]);
+
+        assert_eq!(join_iter.leapfrog_init(), None);
+    }
+
+}
