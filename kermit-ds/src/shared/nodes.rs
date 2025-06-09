a6c0181014ef99df3752e3aa86e869e78a7c51ee
Aidan Bailey <dev@aidanbailey.me>
2025-06-09 12:04:47 +0200

    Doc

--- kermit-ds/src/shared/nodes.rs
+++ kermit-ds/src/shared/nodes.rs
@@ -1,7 +1,14 @@
+//! This module defines any kinds of nodes that could be shared amongst data structures.
 use kermit_iters::key_type::KeyType;
 
+/// The `Node` trait defines a node with an associated value.
 pub trait Node {
+    /// The type of key stored.
     type KT: KeyType;
+
+    /// Creates a new node with the given key.
     fn new(key: Self::KT) -> Self;
+
+    /// Returns a reference to the key.
     fn key(&self) -> &Self::KT;
 }
