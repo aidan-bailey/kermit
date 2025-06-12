615b99978b5520da175303f4f6d7684ccced2bef
Aidan Bailey <dev@aidanbailey.me>
2025-06-12 12:02:03 +0200

    Cut unused db join method

--- kermit/src/utils.rs
+++ kermit/src/utils.rs
@@ -1,7 +1,4 @@
-use {
-    crate::db::Database, kermit_algos::join_algo::JoinAlgo, kermit_ds::relation::Relation,
-    kermit_kvs::keyvalstore::KeyValStore, std::hash::Hash,
-};
+use {kermit_algos::join_algo::JoinAlgo, kermit_ds::relation::Relation};
 
 pub fn compute_join<R, JA>(
     arity: usize, input: Vec<Vec<Vec<R::KT>>>, variables: Vec<usize>,
@@ -17,30 +14,4 @@ where
         .collect();
     let iterables = relations.iter().collect::<Vec<_>>();
     JA::join(variables, rel_variables, iterables)
-}
-
-pub fn compute_db_join<VT, KVST, R, JA>(
-    input1: Vec<Vec<R::KT>>, input2: Vec<Vec<R::KT>>,
-) -> Database<VT, KVST, R>
-where
-    KVST: KeyValStore<R::KT, VT> + Default,
-    VT: Hash,
-    R: Relation,
-    JA: JoinAlgo<R>,
-{
-    let mut db = Database::<VT, KVST, R>::new("test_db".to_string(), KVST::default());
-
-    db.add_relation("first", 1);
-    db.add_keys_batch("first", input1);
-
-    db.add_relation("second", 1);
-    db.add_keys_batch("second", input2);
-
-    let _result = db.join::<JA>(
-        vec!["first".to_string(), "second".to_string()],
-        vec![0],
-        vec![vec![0], vec![0]],
-    );
-
-    db
 }
