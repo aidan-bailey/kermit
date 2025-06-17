007ae19ea4f0b5385143fa131fc506dfafd2d573
Aidan Bailey <dev@aidanbailey.me>
2025-06-17 10:56:59 +0200

    Refactor

--- kermit/src/testing/macros.rs
+++ kermit/src/testing/macros.rs
@@ -2,7 +2,8 @@
 macro_rules! define_join_test {
     (
         $test_name:ident,                // fn name
-        $relation_type:ty,              // RelationTrie<u64>
+        $key_type:ty,
+        $relation_type:ident,              // RelationTrie<u64>
         $join_algorithm:ty,             // LeapfrogTriejoin
         $arity:expr,                    // arity (e.g. 1 or 2)
         [ $( $input:expr ),+ $(,)? ],   // input relations
@@ -12,9 +13,9 @@ macro_rules! define_join_test {
     ) => {
         #[test]
         fn $test_name() {
-            let inputs: Vec<Vec<Vec<u64>>> = vec![$($input.to_vec()),+];
+            let inputs: Vec<Vec<Vec<$key_type>>> = vec![$($input.to_vec()),+];
 
-            $crate::testing::utils::test_join::<$relation_type, $join_algorithm>(
+            $crate::testing::utils::test_join::<$relation_type<$key_type>, $join_algorithm>(
                 $arity,
                 inputs,
                 $join_vars.to_vec(),
@@ -27,21 +28,23 @@ macro_rules! define_join_test {
 
 #[macro_export]
 macro_rules! define_simple_join_test {
-    ($test_name:ident, $relation_type:ty, $join_algorithm:ty) => {
+    ($key_type:ty, $relation_type:ident, $join_algorithm:ty) => {
+        paste::paste! {
         $crate::define_join_test!(
-            $test_name,
+            [<simple_join_ $relation_type:lower _ $join_algorithm:lower _ $key_type:lower>],
+            $key_type,
             $relation_type,
             $join_algorithm,
             1, // Arity (unary relations)
             [
-                // Input relations
-                vec![vec![1u64], vec![2u64], vec![3u64]],
-                vec![vec![1u64], vec![2u64], vec![3u64]]
+                vec![vec![1 as $key_type], vec![2], vec![3]],
+                vec![vec![1], vec![2], vec![3]]
             ],
             vec![0],                                  // Join variables
             vec![vec![0]],                            // Projection
-            vec![vec![1u64], vec![2u64], vec![3u64]]  // Expected output
+            vec![vec![1 as $key_type], vec![2], vec![3]]  // Expected output
         );
+        }
     };
 }
 
@@ -49,19 +52,16 @@ macro_rules! define_simple_join_test {
 macro_rules! define_join_test_suite {
     (
         $(
-            $test_name:ident,                // fn name
-            $relation_type:ty,              // RelationTrie<u64>
+            $relation_type:ident,              // RelationTrie<u64>
             $join_algorithm:ty,             // LeapfrogTriejoin
         ),+
     ) => {
         $(
-            paste::paste! {
                 $crate::define_simple_join_test!(
-                    [<$test_name _simple_join>],
+                    u64,
                     $relation_type,
                     $join_algorithm
                 );
-            }
         )+
     };
 }
