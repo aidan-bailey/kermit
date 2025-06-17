8623e20e3d7138734400a1f10c52745336ea1995
Aidan Bailey <dev@aidanbailey.me>
2025-06-17 12:44:48 +0200

    Refactor

--- kermit/src/testing/macros.rs
+++ kermit/src/testing/macros.rs
@@ -1,5 +1,5 @@
 #[macro_export]
-macro_rules! define_join_test {
+macro_rules! define_multiway_join_test {
     (
         $test_name:ident,                // fn name
         $key_type:ty,
@@ -27,11 +27,11 @@ macro_rules! define_join_test {
 }
 
 #[macro_export]
-macro_rules! define_simple_join_test {
+macro_rules! define_simple_multiway_join_test {
     ($key_type:ty, $relation_type:ident, $join_algorithm:ty) => {
         paste::paste! {
-        $crate::define_join_test!(
-            [<simple_join_ $relation_type:lower _ $join_algorithm:lower _ $key_type:lower>],
+        $crate::define_multiway_join_test!(
+            [<simple_multiwayjoin_ $relation_type:lower _ $join_algorithm:lower _ $key_type:lower>],
             $key_type,
             $relation_type,
             $join_algorithm,
@@ -49,15 +49,15 @@ macro_rules! define_simple_join_test {
 }
 
 #[macro_export]
-macro_rules! define_join_test_suite {
+macro_rules! define_multiway_join_test_suite {
     (
         $(
-            $relation_type:ident,              // RelationTrie<u64>
+            $relation_type:ident,              // RelationTrie
             $join_algorithm:ty,             // LeapfrogTriejoin
         ),+
     ) => {
         $(
-                $crate::define_simple_join_test!(
+                $crate::define_simple_multiway_join_test!(
                     u64,
                     $relation_type,
                     $join_algorithm
