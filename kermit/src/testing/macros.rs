d4a0575d299ce453691b4ab53f6f77143135871b
Aidan Bailey <dev@aidanbailey.me>
2025-06-18 09:46:30 +0200

    Add multiway join test macros

--- kermit/src/testing/macros.rs
+++ kermit/src/testing/macros.rs
@@ -1,15 +1,15 @@
 #[macro_export]
 macro_rules! define_multiway_join_test {
     (
-        $test_name:ident,                // fn name
+        $test_name:ident,
         $key_type:ty,
-        $relation_type:ident,              // RelationTrie<u64>
-        $join_algorithm:ty,             // LeapfrogTriejoin
-        $arity:expr,                    // arity (e.g. 1 or 2)
-        [ $( $input:expr ),+ $(,)? ],   // input relations
-        $join_vars:expr,                // join variable indices
-        $projection:expr,               // projected columns
-        $expected:expr                  // expected result
+        $relation_type:ident,
+        $join_algorithm:ty,
+        $arity:expr,
+        [ $( $input:expr ),+ $(,)? ],
+        $join_vars:expr,
+        $projection:expr,
+        $expected:expr
     ) => {
         #[test]
         fn $test_name() {
@@ -35,14 +35,60 @@ macro_rules! define_simple_multiway_join_test {
             $key_type,
             $relation_type,
             $join_algorithm,
-            1, // Arity (unary relations)
+            1,
             [
-                vec![vec![1 as $key_type], vec![2], vec![3]],
+                vec![vec![1], vec![2], vec![3]],
                 vec![vec![1], vec![2], vec![3]]
             ],
-            vec![0],                                  // Join variables
-            vec![vec![0]],                            // Projection
-            vec![vec![1 as $key_type], vec![2], vec![3]]  // Expected output
+            vec![0],
+            vec![vec![0]],
+            vec![vec![1], vec![2], vec![3]]
+        );
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! define_triangle_multiway_join_test {
+    ($key_type:ty, $relation_type:ident, $join_algorithm:ty) => {
+        paste::paste! {
+        $crate::define_multiway_join_test!(
+            [<triangle_ $relation_type:lower _ $join_algorithm:lower _ $key_type:lower>],
+            $key_type,
+            $relation_type,
+            $join_algorithm,
+            2,
+            [
+                vec![vec![1, 2], vec![2, 3], vec![3, 1]],
+                vec![vec![2, 3], vec![3, 1], vec![1, 2]],
+                vec![vec![3, 1], vec![1, 2], vec![2, 3]]
+            ],
+            vec![0, 1, 2],
+            vec![vec![0, 1], vec![1, 2], vec![2, 3]],
+            vec![vec![1, 2, 3], vec![2, 3, 1], vec![3, 1, 2]]
+        );
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! define_chainjoin_multiway_join_test {
+    ($key_type:ty, $relation_type:ident, $join_algorithm:ty) => {
+        paste::paste! {
+        $crate::define_multiway_join_test!(
+            [<chainjoin_ $relation_type:lower _ $join_algorithm:lower _ $key_type:lower>],
+            $key_type,
+            $relation_type,
+            $join_algorithm,
+            2,
+            [
+                vec![vec![1, 2], vec![2, 3]],
+                vec![vec![2, 4], vec![3, 5]],
+                vec![vec![4, 6], vec![5, 7]]
+            ],
+            vec![0, 1, 2, 3],
+            vec![vec![0, 1], vec![1, 2], vec![2, 3]],
+            vec![vec![1, 2, 4, 6], vec![2, 3, 5, 7]]
         );
         }
     };
@@ -52,12 +98,26 @@ macro_rules! define_simple_multiway_join_test {
 macro_rules! define_multiway_join_test_suite {
     (
         $(
-            $relation_type:ident,              // RelationTrie
-            $join_algorithm:ty,             // LeapfrogTriejoin
+            $relation_type:ident,
+            $join_algorithm:ty,
         ),+
     ) => {
         $(
                 $crate::define_simple_multiway_join_test!(
+                    u64,
+                    $relation_type,
+                    $join_algorithm
+                );
+
+            /*
+                $crate::define_triangle_multiway_join_test!(
+                    u64,
+                    $relation_type,
+                    $join_algorithm
+                );
+            */
+
+                $crate::define_chainjoin_multiway_join_test!(
                     u64,
                     $relation_type,
                     $join_algorithm
