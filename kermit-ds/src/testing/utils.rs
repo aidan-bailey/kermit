07439da5fb82a0ff2ef2a3f3c5fecec133a5f5a4
Aidan Bailey <dev@aidanbailey.me>
2025-06-23 13:06:40 +0200

    Format

--- kermit-ds/src/testing/utils.rs
+++ kermit-ds/src/testing/utils.rs
@@ -1,13 +1,14 @@
-
-use kermit_iters::trie::TrieIterable;
-
-use crate::relation::Relation;
+use {crate::relation::Relation, kermit_iters::trie::TrieIterable};
 
 pub fn test_trie_relation_iteration<DS>(input: Vec<Vec<DS::KT>>)
 where
     DS: Relation + TrieIterable,
 {
     let relation = DS::from_tuples(input.clone());
-    let res = relation.trie_iter().into_iter().map(|x| x.into_iter().cloned().collect::<Vec<_>>()).collect::<Vec<_>>();
+    let res = relation
+        .trie_iter()
+        .into_iter()
+        .map(|x| x.into_iter().cloned().collect::<Vec<_>>())
+        .collect::<Vec<_>>();
     assert_eq!(res.len(), input.len());
-}   
+}
