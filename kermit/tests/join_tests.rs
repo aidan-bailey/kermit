674c1ba72d620c842dd943725162384c8c556dd6
Aidan Bailey <dev@aidanbailey.me>
2025-06-24 09:29:31 +0200

    Format

--- kermit/tests/join_tests.rs
+++ kermit/tests/join_tests.rs
@@ -1,8 +1,7 @@
 mod common;
 
 use {
-    kermit_algos::leapfrog_triejoin::LeapfrogTriejoin,
-    kermit_ds::ds::relation_trie::RelationTrie,
+    kermit_algos::leapfrog_triejoin::LeapfrogTriejoin, kermit_ds::ds::relation_trie::RelationTrie,
 };
 
 define_multiway_join_test_suite!(RelationTrie, LeapfrogTriejoin);
