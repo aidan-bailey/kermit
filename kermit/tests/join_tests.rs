e1a83a20170ed7afc2b2b9696d2e5346c841160d
Aidan Bailey <dev@aidanbailey.me>
2025-06-24 09:20:43 +0200

    Move integration tests

---
+++ kermit/tests/join_tests.rs
@@ -0,0 +1,8 @@
+mod common;
+
+use {
+    kermit_algos::leapfrog_triejoin::LeapfrogTriejoin,
+    kermit_ds::ds::relation_trie::RelationTrie,
+};
+
+define_multiway_join_test_suite!(RelationTrie, LeapfrogTriejoin);
