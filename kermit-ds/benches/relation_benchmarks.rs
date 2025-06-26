2e383dc4e2d401a70e29781e6c9d7a27298428a6
Aidan Bailey <dev@aidanbailey.me>
2025-06-26 12:33:07 +0200

    Decouple tuple generation methods

--- kermit-ds/benches/relation_benchmarks.rs
+++ kermit-ds/benches/relation_benchmarks.rs
@@ -1,97 +1,14 @@
 use {
-    criterion::{
+    common::tuple_generation::{generate_distinct_tuples, generate_exponential_tuples, generate_factorial_tuples}, criterion::{
         criterion_group, criterion_main, measurement::WallTime, BatchSize, BenchmarkGroup,
         Criterion,
-    },
-    kermit_ds::{ds::relation_trie::RelationTrie, relation::Relation},
-    kermit_iters::trie::TrieIterable,
-    num_traits::PrimInt,
-    rand::{
+    }, kermit_ds::{ds::relation_trie::RelationTrie, relation::Relation}, kermit_iters::trie::TrieIterable, num_traits::PrimInt, rand::{
         distr::{uniform::SampleUniform, Uniform},
         rng, Rng,
-    },
-    std::{collections::HashSet, hash::Hash, hint::black_box},
+    }, std::{collections::HashSet, hash::Hash, hint::black_box}
 };
 
-pub fn generate_exponential_tuples<T>(k: T) -> Vec<Vec<T>>
-where
-    T: PrimInt + num_traits::NumCast,
-{
-    let k_usize = num_traits::cast::<T, usize>(k).expect("Failed to cast T to usize");
-    let mut tuples: Vec<Vec<T>> = vec![];
-
-    let tuple = (0..k_usize)
-        .map(|_| num_traits::cast::<usize, T>(0).unwrap())
-        .collect::<Vec<T>>();
-
-    fn recurse<T>(k_curr: usize, k: usize, current: Vec<T>, result: &mut Vec<Vec<T>>)
-    where
-        T: PrimInt + num_traits::NumCast,
-    {
-        if k_curr == k {
-            result.push(current);
-            return;
-        }
-
-        for i in 0..k {
-            let mut new_tuple = current.clone();
-            new_tuple.push(num_traits::cast::<usize, T>(i).unwrap());
-            recurse(k_curr + 1, k, new_tuple, result);
-        }
-    }
-
-    recurse(0, k_usize, tuple, &mut tuples);
-
-    tuples
-}
-
-pub fn generate_factorial_tuples<T>(k: T) -> Vec<Vec<T>>
-where
-    T: PrimInt + num_traits::NumCast,
-{
-    let k_usize = num_traits::cast::<T, usize>(k).expect("Failed to cast T to usize");
-    let mut tuples: Vec<Vec<T>> = vec![];
-
-    let tuple = (0..k_usize)
-        .map(|_| num_traits::cast::<usize, T>(0).unwrap())
-        .collect::<Vec<T>>();
-
-    fn recurse<T>(k_curr: usize, k: usize, current: Vec<T>, result: &mut Vec<Vec<T>>)
-    where
-        T: PrimInt + num_traits::NumCast,
-    {
-        if k_curr == k {
-            result.push(current);
-            return;
-        }
-
-        for i in 0..=k_curr {
-            let mut new_tuple = current.clone();
-            new_tuple.push(num_traits::cast::<usize, T>(i).unwrap());
-            recurse(k_curr + 1, k, new_tuple, result);
-        }
-    }
-
-    recurse(0, k_usize, tuple, &mut tuples);
-
-    tuples
-}
-
-pub fn generate_distinct_tuples<T>(n: usize, k: usize) -> Vec<Vec<T>>
-where
-    T: PrimInt + SampleUniform + Hash,
-{
-    let mut set = HashSet::new();
-    let mut rng = rng();
-    let dist = Uniform::new(T::min_value(), T::max_value()).ok().unwrap();
-
-    while set.len() < n {
-        let tuple: Vec<T> = (0..k).map(|_| rng.sample(&dist)).collect();
-        set.insert(tuple);
-    }
-
-    set.into_iter().collect()
-}
+mod common;
 
 fn bench_relation_insert<R: Relation>(group: &mut BenchmarkGroup<WallTime>)
 where
