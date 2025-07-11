86fe751e4e2ad6d7a75473026a396bce2a36d96f
Aidan Bailey <dev@aidanbailey.me>
2025-07-11 15:44:44 +0200

    Add `Display` constraint to `KeyType`

--- kermit-iters/src/key_type.rs
+++ kermit-iters/src/key_type.rs
@@ -2,8 +2,8 @@
 //! for a key type used in relations. NOTE: Yes, the number of traits is large
 //! and probably not necessary.
 
-use std::fmt::Debug;
+use std::fmt::{Debug, Display};
 
 /// Trait for key types used in relations.
-pub trait KeyType: Debug + Ord + Copy {}
-impl<KT> KeyType for KT where KT: Debug + Ord + Copy {}
+pub trait KeyType: Debug + Ord + Copy + Display {}
+impl<KT> KeyType for KT where KT: Debug + Ord + Copy + Display {}
