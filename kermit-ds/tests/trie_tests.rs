058881bafea358712c6f901bcdb72eef2ae10631
Aidan Bailey <dev@aidanbailey.me>
2025-07-15 11:18:54 +0200

    Add col trie unit tests

--- kermit-ds/tests/trie_tests.rs
+++ kermit-ds/tests/trie_tests.rs
@@ -1,4 +1,7 @@
 use kermit_ds::ds::relation_trie::RelationTrie;
+use kermit_ds::ds::column_trie::ColumnTrie;
 mod common;
 
 relation_trie_test_suite!(RelationTrie);
+
+relation_trie_test_suite!(ColumnTrie);
