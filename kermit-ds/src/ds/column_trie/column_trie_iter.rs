6ec1935db4806e0d65db5f65455744564ffaa382
Aidan Bailey <dev@aidanbailey.me>
2025-07-15 11:17:56 +0200

    Add column trie iter implementation

---
+++ kermit-ds/src/ds/column_trie/column_trie_iter.rs
@@ -0,0 +1,192 @@
+use {
+    super::implementation::{ColumnTrie, ColumnTrieLayer},
+    kermit_derive::IntoTrieIter, // Ensure TrieIteratorWrapper is imported
+    kermit_iters::{
+        key_type::KeyType,
+        linear::LinearIterator,
+        trie::{TrieIterable, TrieIterator, TrieIteratorWrapper},
+    },
+};
+
+#[derive(IntoTrieIter)]
+pub struct ColumnTrieIter<'a, KT: KeyType> {
+    /// Index of current layer.
+    layer_number: usize,
+    /// Index of interval start at current layer
+    interval_i: usize,
+    /// Relative index based on interval
+    rel_data_i: usize,
+    /// Relative data at current layer
+    rel_data: Option<&'a [KT]>,
+    /// The trie being iterated.
+    trie: &'a ColumnTrie<KT>,
+}
+
+impl<'a, KT: KeyType> ColumnTrieIter<'a, KT> {
+    pub fn at_root(&self) -> bool { self.layer_number == 0 }
+
+    pub fn new(trie: &'a ColumnTrie<KT>) -> Self {
+        ColumnTrieIter {
+            interval_i: 0,
+            rel_data: None,
+            rel_data_i: 0,
+            layer_number: 0,
+            trie,
+        }
+    }
+}
+
+impl<KT: KeyType> LinearIterator for ColumnTrieIter<'_, KT> {
+    type KT = KT;
+
+    fn key(&self) -> Option<Self::KT> {
+        if let Some(data) = self.rel_data {
+            data.get(self.rel_data_i).copied()
+        } else {
+            None
+        }
+    }
+
+    fn next(&mut self) -> Option<Self::KT> {
+        if let Some(data) = self.rel_data
+            && self.rel_data_i < data.len()
+        {
+            self.rel_data_i += 1;
+            data.get(self.rel_data_i).copied()
+        } else {
+            None
+        }
+    }
+
+    fn seek(&mut self, seek_key: Self::KT) -> bool {
+        while let Some(key) = self.next()
+            && key < seek_key
+        {}
+        self.at_end()
+    }
+
+    fn at_end(&self) -> bool {
+        if let Some(data) = self.rel_data {
+            self.rel_data_i >= data.len()
+        } else {
+            true
+        }
+    }
+}
+
+impl<KT: KeyType> TrieIterator for ColumnTrieIter<'_, KT> {
+    fn open(&mut self) -> bool {
+        if self.layer_number == self.trie.arity {
+            // If at leaf, return false
+            false
+        } else if self.layer_number == 0 {
+            let next_layer = self.trie.layer(0);
+            if next_layer.data.is_empty() {
+                // If the first layer is empty, we are in an empty trie and return false
+                return false;
+            }
+            // If at root, initialize the first layer
+            self.layer_number = 1;
+            self.rel_data_i = 0;
+            self.interval_i = 0;
+            self.rel_data = Some(&self.trie.layer(0).data);
+            true
+        } else {
+            // If not at root or leaf, compute next interval index
+            let curr_layer = self.trie.layer(self.layer_number - 1);
+            let prev_start_index = curr_layer.interval[self.interval_i];
+            self.interval_i = prev_start_index + self.rel_data_i;
+            // increment layer number
+            self.layer_number += 1;
+            // get new layer
+            let next_layer = self.trie.layer(self.layer_number - 1);
+            // get next start and end indices
+            let next_start_index = next_layer.interval[self.interval_i];
+            let next_end_index = if self.interval_i + 1 < next_layer.interval.len() {
+                next_layer.interval[self.interval_i + 1]
+            } else {
+                next_layer.data.len()
+            };
+            // set new relative data
+            self.rel_data = Some(&next_layer.data[next_start_index..next_end_index]);
+            self.rel_data_i = 0;
+            true
+        }
+    }
+
+    fn up(&mut self) -> bool {
+        if self.layer_number == 0 {
+            // If already at root, cannot go up
+            false
+        } else if self.layer_number == 1 {
+            // If moving to root, reset all indices
+            self.layer_number = 0;
+            self.interval_i = 0;
+            self.rel_data_i = 0;
+            self.rel_data = None;
+            true
+        } else {
+            // If moving up, decrement layer index
+            self.layer_number -= 1;
+            let layer = self.trie.layer(self.layer_number - 1);
+            // Our global data index is interval_i, so we must find the start index
+
+            // Data index of parent is equivalent to current interval index
+            let data_index = self.interval_i;
+            // We need to find the interval index of the data in the previous layer
+            // The start indexes are ordered, so we need to find the point at which the
+            // data index is less than the current start index. Then the previous index 
+            // is our new interval index
+            for (i, start_index) in layer.interval.iter().enumerate() {
+                if data_index < *start_index {
+                    break
+                } else {
+                    self.interval_i = i;
+                }
+            }
+
+            // Our new start index is at the new interval index
+            let start_index = layer.interval[self.interval_i];
+            // The end index is either the next start index, or the length of the data
+            let end_index = if self.interval_i + 1 < layer.interval.len() {
+                layer.interval[self.interval_i + 1]
+            } else {
+                layer.data.len()
+            };
+
+            // Set new relative data
+            self.rel_data = Some(&layer.data[start_index..end_index]);
+            self.rel_data_i = data_index - start_index;
+
+            true
+        }
+    }
+}
+
+/// Implementation of the `TrieIterable` trait for `RelationTrie`.
+impl<KT: KeyType> TrieIterable for ColumnTrie<KT> {
+    fn trie_iter(&self) -> impl TrieIterator<KT = KT> + IntoIterator<Item = Vec<KT>> {
+        ColumnTrieIter::new(self)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use {
+        super::*,
+        crate::relation::{Relation, RelationBuilder},
+        kermit_iters::key_type::KeyType,
+    };
+
+    #[test]
+    fn test_insert() {
+        let mut trie = ColumnTrie::<usize>::builder(3)
+            .add_tuples(vec![vec![1, 2, 3], vec![4, 5, 6]])
+            .build();
+        println!("{trie}");
+        let mut t = trie.trie_iter();
+
+        let res = t.into_iter().collect::<Vec<_>>();
+    }
+
+}
