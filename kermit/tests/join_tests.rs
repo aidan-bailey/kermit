c1c2b61c822585632dbe91ee20b46b016ee4aeab
Aidan Bailey <dev@aidanbailey.me>
2025-07-15 11:22:32 +0200

    Add column trie lftj tests

--- kermit/tests/join_tests.rs
+++ kermit/tests/join_tests.rs
@@ -1,7 +1,9 @@
 mod common;
 
 use {
-    kermit_algos::leapfrog_triejoin::LeapfrogTriejoin, kermit_ds::ds::relation_trie::RelationTrie,
+    kermit_algos::leapfrog_triejoin::LeapfrogTriejoin, kermit_ds::ds::{relation_trie::RelationTrie, column_trie::ColumnTrie},
 };
 
 define_multiway_join_test_suite!(RelationTrie, LeapfrogTriejoin);
+
+define_multiway_join_test_suite!(ColumnTrie, LeapfrogTriejoin);
