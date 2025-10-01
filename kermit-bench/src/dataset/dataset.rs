6f1d50549198af457f3eda5559d171df3c879991
Aidan Bailey <dev@aidanbailey.me>
2025-10-01 16:06:51 +0200

    Change structs

--- kermit-bench/src/dataset/dataset.rs
+++ kermit-bench/src/dataset/dataset.rs
@@ -1,10 +1,30 @@
-pub trait DatasetMetadata {
-    fn name(&self) -> &str;
-    fn description(&self) -> &str;
-    fn url(&self) -> &str;
+use {super::downloader::DownloadSpec, std::path::Path};
+
+pub struct DatasetMetadata {
+    name: &'static str,
+    description: &'static str,
+    download_spec: DownloadSpec,
 }
 
-pub trait Dataset {
-    fn metadata(&self) -> impl DatasetMetadata;
-    fn load(&self) -> Result<(), Box<dyn std::error::Error>>;
+impl DatasetMetadata {
+    pub const fn new(
+        name: &'static str, description: &'static str, download_spec: DownloadSpec,
+    ) -> Self {
+        Self {
+            name,
+            description,
+            download_spec,
+        }
+    }
+
+    pub fn name(&self) -> &'static str { self.name }
+
+    pub fn description(&self) -> &'static str { self.description }
+
+    pub fn download_spec(&self) -> &DownloadSpec { &self.download_spec }
+}
+
+pub trait DatasetTrait {
+    fn metadata(&self) -> &DatasetMetadata;
+    fn load(&self, path: &Path) -> Result<(), Box<dyn std::error::Error>>;
 }
