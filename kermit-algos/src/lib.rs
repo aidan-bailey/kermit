700a7b0930abf0c02fed629efb69344b1ee52b13
Aidan Bailey <dev@aidanbailey.me>
2025-10-30 15:48:03 +0200

    Move join query to kermit-algos

--- kermit-algos/src/lib.rs
+++ kermit-algos/src/lib.rs
@@ -1,5 +1,6 @@
 mod join_algo;
 mod leapfrog_join;
 mod leapfrog_triejoin;
+mod join_query;
 
-pub use {join_algo::JoinAlgo, leapfrog_triejoin::LeapfrogTriejoin};
+pub use {join_algo::JoinAlgo, leapfrog_triejoin::LeapfrogTriejoin, join_query::JoinQuery};
