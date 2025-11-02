8ec48a85e941544b4da57d96abbf6276b1fac0de
Aidan Bailey <dev@aidanbailey.me>
2025-11-02 15:48:51 +0200

    Add JoinAlgorithm enum

--- kermit-algos/src/lib.rs
+++ kermit-algos/src/lib.rs
@@ -6,3 +6,20 @@ mod queries;
 pub use {
     join_algo::JoinAlgo, leapfrog_triejoin::LeapfrogTriejoin, queries::join_query::JoinQuery,
 };
+use {clap::ValueEnum, std::str::FromStr};
+
+#[derive(Copy, Clone, PartialEq, Eq, Debug, ValueEnum)]
+pub enum JoinAlgorithm {
+    LeapfrogTriejoin,
+}
+
+impl FromStr for JoinAlgorithm {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            | "leapfrog_triejoin" => Ok(Self::LeapfrogTriejoin),
+            | _ => Err(format!("Invalid join algorithm: {}", s).into()),
+        }
+    }
+}
