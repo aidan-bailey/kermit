8afe67f0d2a8a7b1816c0a36c8de7db592e6db93
Aidan Bailey <dev@aidanbailey.me>
2025-11-02 14:31:23 +0200

    Refactor from name

--- kermit-bench/src/benchmarks/mod.rs
+++ kermit-bench/src/benchmarks/mod.rs
@@ -9,11 +9,16 @@ pub enum Benchmark {
 
 impl Benchmark {
 
-    pub fn from_name(name: &str) -> Option<Self> {
-        match name {
-            | "oxford" => Some(Self::Oxford),
-            | _ => None,
+    pub fn from_name(name: &str) -> Result<Self, Box<dyn std::error::Error>> {
+        if name == Benchmark::Oxford.name() {
+            Ok(Self::Oxford)
+        } else {
+            Err(format!("Benchmark '{}' not found", name).into())
         }
+    }
+
+    pub fn name(self) -> String {
+        self.config().metadata().name.to_string()
     }
 
     pub fn config(self) -> Box<dyn BenchmarkConfig + 'static> {
