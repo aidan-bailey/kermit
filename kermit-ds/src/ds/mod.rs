ec12863ae0aca36dba91d16968488e7e165d7acc
Aidan Bailey <dev@aidanbailey.me>
2025-11-02 15:32:44 +0200

    Add IndexStructure enum

--- kermit-ds/src/ds/mod.rs
+++ kermit-ds/src/ds/mod.rs
@@ -2,3 +2,22 @@ mod column_trie;
 mod tree_trie;
 
 pub use {column_trie::ColumnTrie, tree_trie::TreeTrie};
+use {clap::ValueEnum, std::str::FromStr};
+
+#[derive(Copy, Clone, PartialEq, Eq, Debug, ValueEnum)]
+pub enum IndexStructure {
+    ColumnTrie,
+    TreeTrie,
+}
+
+impl FromStr for IndexStructure {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            | "column_trie" => Ok(Self::ColumnTrie),
+            | "tree_trie" => Ok(Self::TreeTrie),
+            | _ => Err(format!("Invalid index structure: {}", s).into()),
+        }
+    }
+}
