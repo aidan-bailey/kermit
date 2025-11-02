cbf1f58aa5063666296589063473d4d00e668911
Aidan Bailey <dev@aidanbailey.me>
2025-11-02 15:36:34 +0200

    Add IndexStructure enum

--- kermit-ds/src/lib.rs
+++ kermit-ds/src/lib.rs
@@ -6,3 +6,6 @@ pub use {
     ds::{ColumnTrie, TreeTrie},
     relation::{Projectable, Relation, RelationFileExt, RelationHeader},
 };
+
+// Re-export IndexStructure for external crates (CLI) to reference directly
+pub use ds::IndexStructure;
