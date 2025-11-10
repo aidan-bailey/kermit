745c539630c5eef462d09e87d8f64b27ba5ea8a8
Aidan Bailey <dev@aidanbailey.me>
2025-11-10 11:27:57 +0200

    Format

--- kermit/src/lib.rs
+++ kermit/src/lib.rs
@@ -10,11 +10,8 @@ pub mod db;
 
 pub mod benchmarker;
 
-use {
-    kermit_algos::{JoinAlgo, JoinQuery},
-    kermit_ds::Relation,
-    std::collections::HashMap,
-};
+use {kermit_algos::{JoinAlgo, JoinQuery}, kermit_ds::Relation};
+use std::collections::HashMap;
 
 pub fn compute_join<R, JA>(
     input: Vec<Vec<Vec<usize>>>, variables: Vec<usize>, rel_variables: Vec<Vec<usize>>,
@@ -34,6 +31,7 @@ where
             R::from_tuples(k.into(), tuples)
         })
         .collect();
+
     // Build a synthetic query and datastructure map from the legacy inputs
     // Head: Q(V0, V1, ...)
     let head_vars: Vec<String> = variables.iter().map(|v| format!("V{}", v)).collect();
@@ -43,14 +41,15 @@ where
         let var_list = if rv.is_empty() {
             "_".to_string()
         } else {
-            rv.iter()
-                .map(|v| format!("V{}", v))
-                .collect::<Vec<_>>()
-                .join(", ")
+            rv.iter().map(|v| format!("V{}", v)).collect::<Vec<_>>().join(", ")
         };
         body_preds.push(format!("R{}({})", i, var_list));
     }
-    let query_str = format!("Q({}) :- {}.", head_vars.join(", "), body_preds.join(", "));
+    let query_str = format!(
+        "Q({}) :- {}.",
+        head_vars.join(", "),
+        body_preds.join(", ")
+    );
     let query: JoinQuery = query_str.parse().expect("Failed to build JoinQuery");
 
     // Map datastructures by the synthetic names
