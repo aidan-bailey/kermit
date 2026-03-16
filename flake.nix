8449f1007cebacc5054ce81b89947e1627371c39
Aidan Bailey <dev@aidanbailey.me>
2026-03-16 12:39:45 +0200

    build: add nix flake with dev shell

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "Kermit – relational algebra research and benchmarking";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    rust-overlay,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        rustToolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            rustToolchain
+            pkgs.git-cliff
+            pkgs.cargo-expand
+          ];
+
+          MIRIFLAGS = "-Zmiri-disable-isolation";
+          RUST_BACKTRACE = "1";
+        };
+      }
+    );
+}
